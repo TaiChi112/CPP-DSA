12a96e95a234f0f8d878705c4d27047e031cdc56
TaiChi <anothai.0978452316@gmail.com>
2024-02-12 14:15:14 +0700

    Loop input and output value in the array successsfully

--- DSA/DSA/Array/main.cpp
+++ DSA/DSA/Array/main.cpp
@@ -8,5 +8,18 @@ int main()
     cout << "Create size of array : ";
     cin >> sizeOfArray;
     cout << "Size of array your input : " << sizeOfArray << endl;
+    int ArrayOfNumbers[sizeOfArray];
+
+    for (int i = 0; i < sizeOfArray; i++)
+    {
+        cout << "Input number of array : ";
+        cin >> ArrayOfNumbers[i];
+    }
+
+    for (int i = 0; i < sizeOfArray; i++)
+    {
+        cout << "Display value in the Array of numbers : " << ArrayOfNumbers[i] << endl;
+    }
+
     return 0;
 }
