479df0d1ef9718a22ef09b80809ef961ba780939
TaiChi <anothai.0978452316@gmail.com>
2024-02-12 13:55:40 +0700

    Test of create size of array

---
+++ DSA/DSA/Array/array.md
@@ -0,0 +1,6 @@
+## quick CRUD of array
+    - create size of array
+    - read array
+    - update array
+    - delete array
+    
---
+++ DSA/DSA/Array/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int sizeOfArray;
+    cout << "Create size of array : ";
+    cin >> sizeOfArray;
+    cout << "Size of array your input : " << sizeOfArray << endl;
+    return 0;
+}
