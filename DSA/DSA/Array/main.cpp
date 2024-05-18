258102eb8360d0e289abd0d83de3cfdbe26d9294
TaiChi <anothai.0978452316@gmail.com>
2024-05-18 22:14:46 +0700

    Garbage : sos soys 112 git push origin tutorial C++

--- DSA/DSA/Array/main.cpp
+++ DSA/DSA/Array/main.cpp
@@ -2,24 +2,22 @@
 
 using namespace std;
 
+// int lenght(int data[], int size)
+// {
+//     return sizeof(data) / sizeof(data[0]);
+// }
+// void display(int data[])
+// {
+//     for (int i = 0; i < lenght(data); i++)
+//     {
+//         cout << "Data in array index : " << data[i] << endl;
+//     }
+// }
 int main()
 {
-    int sizeOfArray;
-    cout << "Create size of array : ";
-    cin >> sizeOfArray;
-    cout << "Size of array your input : " << sizeOfArray << endl;
-    int ArrayOfNumbers[sizeOfArray];
-
-    for (int i = 0; i < sizeOfArray; i++)
-    {
-        cout << "Input number of array : ";
-        cin >> ArrayOfNumbers[i];
-    }
-
-    for (int i = 0; i < sizeOfArray; i++)
-    {
-        cout << "Display value in the Array of numbers : " << ArrayOfNumbers[i] << endl;
-    }
-
+    int data[5] = {1, 2, 3, 4, 5};
+    cout << "size of array : " << sizeof(data) << " bytes" << endl;
+    cout << "size of array element : " << sizeof(data[0]) << " bytes" << endl;
+    // display(data);
     return 0;
 }
