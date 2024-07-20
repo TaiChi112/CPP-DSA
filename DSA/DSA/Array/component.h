4c97212f4f1ec50e84f65089d0a17b208fd6c0eb
TaiChi <anothai.0978452316@gmail.com>
2024-07-20 16:32:24 +0700

    feat : define field and constructor,destructor prototype

--- DSA/DSA/Array/component.h
+++ DSA/DSA/Array/component.h
@@ -1,4 +1,17 @@
 #ifndef COMPONENT_H
 #define COMPONENT_H
 
+class Array{
+    private:
+        int *A;
+        int size;
+        int length;
+        void Swap(int *x, int *y);
+    public:
+        Array();
+        Array(int size);
+        ~Array();
+        
+};
+
 #endif
