4fbf316274e00789890183566e9a9625bb3bca08
TaiChi <anothai.0978452316@gmail.com>
2024-07-20 16:34:00 +0700

    chore : assign detail construtor

--- DSA/DSA/Array/component.cpp
+++ DSA/DSA/Array/component.cpp
@@ -1,2 +1,14 @@
 #include "component.h"
+#include <iostream>
 
+using namespace std;
+
+Array::Array() : size(10), length(0)
+{
+    A = new int[size];
+}
+
+Array::Array(int size) : size(size), length(0)
+{
+    A = new int[size];
+}
