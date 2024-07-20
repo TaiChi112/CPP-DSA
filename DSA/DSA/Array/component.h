e9bd264ca012613affb9db2152c5adf80d4df37b
TaiChi <anothai.0978452316@gmail.com>
2024-07-20 21:28:22 +0700

    chore : addition struct for compare with class

--- DSA/DSA/Array/component.h
+++ DSA/DSA/Array/component.h
@@ -1,17 +1,28 @@
 #ifndef COMPONENT_H
 #define COMPONENT_H
 
-class Array{
+class Array1{
     private:
         int *A;
         int size;
         int length;
         void Swap(int *x, int *y);
     public:
-        Array();
-        Array(int size);
-        ~Array();
+        Array1();
+        Array1(int size);
+        ~Array1();
+        void Display();
         
 };
+struct Array2
+{
+    int *A;
+    int size;
+    int length;
+    Array2();
+    Array2(int size);
+    ~Array2();
+};
+
 
 #endif
