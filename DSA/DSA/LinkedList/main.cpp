c08fadaf12f4cfd1cbe76279965f7ed9f2bb0494
TaiChi <anothai.0978452316@gmail.com>
2024-07-31 01:27:27 +0700

    test : Use struct normal assignment & dynamic assignment

--- DSA/DSA/LinkedList/main.cpp
+++ DSA/DSA/LinkedList/main.cpp
@@ -8,5 +8,12 @@ struct Node
 };
 
 int main(){
+    Node *node1{new Node()};
+    node1->data = 100;
+    node1->address = nullptr;
+
+    Node node2;
+    node2.data = 112;
+    node2.address = nullptr;
     return 0;
 }
