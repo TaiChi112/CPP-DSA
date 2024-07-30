b399cfa04f1801681b7783224dee1506807ddef8
TaiChi <anothai.0978452316@gmail.com>
2024-07-31 01:39:02 +0700

    test : normal constructor before test dynamic constructor finished!

--- DSA/DSA/LinkedList/main.cpp
+++ DSA/DSA/LinkedList/main.cpp
@@ -8,20 +8,40 @@ struct Node
     Node(){};
     Node(int data) : data(data), address(nullptr){};
     Node(int data, Node *address) : data(data), address(address){};
+
+    void display(Node *node)
+    {
+        while (node != nullptr)
+        {
+            cout << node->data << " ";
+            node = node->address;
+        }
+        cout << endl;
+    }
 };
 
 int main(){
     Node *node1{new Node()};
     node1->data = 100;
     node1->address = nullptr;
+    node1->display(node1);
 
     Node node2;
     node2.data = 112;
     node2.address = nullptr;
+    node2->display(&node2);
 
     Node *node3{new Node(113)};
     node3->address = nullptr;
+    node3->display(node3);
 
-    Node *node4{new Node(114, nullptr)};
+    Node node4{113, nullptr};
+    node3.display(&node4);
+
+    Node *node5{new Node(114, nullptr)};
+    node5->display(node5);
+
+    Node node6{115, nullptr};
+    node6.display(&node6);
     return 0;
 }
