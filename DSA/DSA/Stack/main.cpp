8f4a42d7a3a630feb3dfa3d9934a89da7ae0dc6b
TaiChi <anothai.0978452316@gmail.com>
2024-08-02 01:12:20 +0700

    feat : function  name push for push data in Stack

--- DSA/DSA/Stack/main.cpp
+++ DSA/DSA/Stack/main.cpp
@@ -4,17 +4,36 @@ using namespace std;
 
 struct Node
 {
-    int data;
-    Node *next;
+      int data;
+      Node *next;
+      Node() {}
+      Node(int value) : data(value), next(nullptr) {}
 };
-class Stack{
-      private:
-        Node *top;
-    public:
-    Stack():top(nullptr){}
-        
-};
-int main(){
+class Stack
+{
+private:
+      Node *top;
 
-    return 0;
+public:
+      Stack() : top(nullptr) {}
+
+      void push(int value)
+      {
+            Node *newNode = new Node(value);
+            if (top == nullptr)
+            {
+                  top = newNode;
+            }
+            else
+            {
+                  newNode->next = top;
+                  top = newNode;
+            }
+            cout << "Pushed : " << value << endl;
+      }
+};
+int main()
+{
+
+      return 0;
 }
