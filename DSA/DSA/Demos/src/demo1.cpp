2bfa1a0baf0ab213d618690d181bfe884ec907e9
TaiChi <anothai.0978452316@gmail.com>
2024-08-07 01:49:46 +0700

    learn : add comment code a little bit

--- DSA/DSA/Demos/src/demo1.cpp
+++ DSA/DSA/Demos/src/demo1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-      int value;
+      int value; // variable value it's have only address but it's not assgined yet
       input(&value);
       output(&value);
       return 0;
