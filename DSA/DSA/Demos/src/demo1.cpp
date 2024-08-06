ab83a0d1d4451fee9e331252348e138f88a4f817
TaiChi <anothai.0978452316@gmail.com>
2024-08-07 01:59:29 +0700

    learn : new fn all I/O testing

--- DSA/DSA/Demos/src/demo1.cpp
+++ DSA/DSA/Demos/src/demo1.cpp
@@ -5,8 +5,15 @@ using namespace std;
 
 int main()
 {
-      int value; // variable value it's have only address but it's not assgined yet
-      input(&value);
-      output(&value);
+      // int value; // variable value it's have only address but it's not assgined yet
+      // input(&value);
+      // output(&value);
+      int day, hour, minute, second;
+      // Input_Day(&day);
+      Input_Hour(&hour);
+      Input_Minute(&minute);
+      Input_Second(&second);
+      cout << "Today you use the time : " << hour << " : hour " << minute << " : minute " << second << " : second" << endl;
+      // system("Hello world!");
       return 0;
 }
